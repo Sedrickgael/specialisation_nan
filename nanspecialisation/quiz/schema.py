2a2addccfafc860ff9358d058b64f57060120f78
Gbane26 <34173224+Gbane26@users.noreply.github.com>
2019-11-19 14:39:29 +0000

    App Quiz
    Application

---
+++ nanspecialisation/quiz/schema.py
@@ -0,0 +1,191 @@
+import graphene
+from graphene import relay, ObjectType , Connection 
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
+
+from . import models
+from django.contrib.auth.models import User
+
+class ExtendConnection(Connection):
+    class Meta:
+        abstract = True
+    total_count = graphene.Int()
+    edge_count = graphene.Int()
+    def resolve_total_count(root,info,**kwargs):
+        return root.length
+    def resolve_edge_count(root,info,**kwargs):
+        return len(root.edges)
+
+class SpecialisationNode(DjangoObjectType):
+    class Meta:
+        model = models.Specialisation
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'nom': ['exact', 'icontains', 'istartswith'],
+            'langage': ['exact', 'icontains', 'istartswith'],
+            'statut': ['exact'], 
+            
+        }
+        interfaces = (relay.Node,)
+        connection_class = ExtendConnection
+
+class RelayCreateSpecialisation(graphene.relay.ClientIDMutation):
+    specialisation = graphene.Field(SpecialisationNode)
+    class Input:
+        nom = graphene.String()
+        langage = graphene.String()
+        status = graphene.Boolean()
+        pk = graphene.ID()
+
+    def mutate_and_get_payload(root,info,**kwargs):
+        pk = kwargs.get('id') or None
+        nom = kwargs.get('nom') or None
+        langage = kwargs.get('nom') or None
+        status = kwargs.get('status') or None
+        if nom is not None and status is not None and langage is not None and pk is None:
+            special = models.Specialisation(nom=nom, langage=langage, status=status)
+        elif nom is not None and status is not None and langage is not None and pk is not None :
+            special = models.Specialisation.objects.get(pk=pk)
+            special.nom = nom
+            special.langage = langage
+            special.status = status
+        elif nom is not None and  langage is not None and status is None and pk is not None:
+            special = models.Specialisation.objects.get(pk=pk)
+            special.nom = nom
+            special.langage = langage
+        elif nom is None and langage is not None and status is not None and pk is not None:
+            special = models.Specialisation.objects.get(pk=pk)
+            special.langage = langage
+            special.status = status
+        elif nom is not None and langage is None and status is not None and pk is not None:
+            special = models.Specialisation.objects.get(pk=pk)
+            special.nom = nom
+            special.status = status
+        elif nom is None and langage is None and status is not None and pk is not None:
+            special = models.Specialisation.objects.get(pk=pk)
+            special.status = status
+        else:
+            raise Exception('must be give parameters for Spécialisation mutations')
+        special.save()
+        return RelayCreateSpecialisation(specialisation = special)
+
+class UserNode(DjangoObjectType):
+    class Meta:
+        model = User
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'username': ['exact', 'icontains', 'istartswith'],
+            'statut': ['exact'], 
+        }
+        interfaces = (relay.Node,)
+        connection_class = ExtendConnection
+
+
+class ProfileNode(DjangoObjectType):
+    class Meta:
+        model = models.Profile
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'statut': ['exact'], 
+        }
+        interfaces = (relay.Node, )
+        
+        
+class QuizzNode(DjangoObjectType):
+    class Meta:
+        model = models.Quizz
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'titre': ['exact', 'icontains', 'istartswith'],
+            'niveau': ['exact'],
+            'pourcentage': ['exact'],
+            'nbq': ['exact'],
+            'duree': ['exact'],
+            'statut': ['exact'], 
+        }
+        interfaces = (relay.Node, )
+        connection_class = ExtendConnection
+       
+class QuestionNode(DjangoObjectType):
+    class Meta:
+        model = models.Question
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'niveau': ['exact'],
+            'contenu': ['exact', 'icontains', 'istartswith'],
+            'statut': ['exact'], 
+        }
+        interfaces = (relay.Node,)
+        connection_class = ExtendConnection
+
+
+class ReponseNode(DjangoObjectType):
+    class Meta:
+        model = models.Reponse
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'contenu': ['exact', 'icontains', 'istartswith'],
+            'isrtue': ['exact', 'icontains', 'istartswith'],
+            'statut': ['exact'], 
+        } 
+        interfaces = (relay.Node,)
+        connection_class = ExtendConnection
+
+        
+        
+class QuizzUserNode(DjangoObjectType):
+    class Meta:
+        model = models.QuizzUser
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'note': ['exact'],
+            'statut': ['exact'], 
+        }
+        interfaces = (relay.Node,)
+        connection_class = ExtendConnection
+
+        
+class ReponseUserNode(DjangoObjectType):
+    class Meta:
+        model = models.ReponseUser
+        # Allow for some more advanced filtering here
+        fields = "__all__"
+        filter_fields = {
+            'istrue': ['exact'],
+            'statut': ['exact'], 
+        }
+        interfaces = (relay.Node,)
+        connection_class = ExtendConnection
+
+class Query(graphene.ObjectType):
+    Specialisation = relay.Node.Field(SpecialisationNode)
+    all_Specialisation = DjangoFilterConnectionField(SpecialisationNode)
+
+    Profile = relay.Node.Field(ProfileNode)
+    all_Profiles = DjangoFilterConnectionField(ProfileNode)
+
+    Quizz = relay.Node.Field(QuizzNode)
+    all_Quizzs = DjangoFilterConnectionField(QuizzNode)
+
+    Question = relay.Node.Field(QuestionNode)
+    all_Questions = DjangoFilterConnectionField(QuestionNode)
+
+    Reponse = relay.Node.Field(ReponseNode)
+    all_Reponses = DjangoFilterConnectionField(ReponseNode)
+
+    QuizzUser = relay.Node.Field(QuizzUserNode)
+    all_QuizzUsers = DjangoFilterConnectionField(QuizzUserNode)
+
+    ReponseUser = relay.Node.Field(ReponseUserNode)
+    all_ReponseUsers = DjangoFilterConnectionField(ReponseUserNode)
+
+class RelayMutation(graphene.AbstractType):
+    relay_create_specialisation = RelayCreateSpecialisation.Field()
---
+++ nanspecialisation/quiz/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.connexion, name='login'),
+    path('post', views.islogin, name='login_request'),
+    path('home', views.home, name='home'),
+    path('profil', views.profil, name='profil'),
+    path('quizz', views.quizz, name='quizz'),
+    path('epreuve', views.epreuve, name='epreuve'),
+    path('lesson', views.lesson, name='lesson'),
+    path('deconnexion', views.deconnexion, name='deconnexion'),
+]
