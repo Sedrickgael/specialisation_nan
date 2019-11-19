df6f902273efba96cefbda16ebfd87892552ba44
Gbane26 <34173224+Gbane26@users.noreply.github.com>
2019-11-19 17:08:28 +0000

    url and views

--- nanspecialisation/specialisation/urls.py
+++ nanspecialisation/specialisation/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 
 urlpatterns = [
-    #path('', views.home, name = ''),
+    path('', views.islogin, name = 'home'),
+    path('index', views.index, name = 'index'),
+    path('coding', views.coding, name = 'coding'),
+    path('epreuve', views.epreuve, name = 'epreuve'),
+    path('lesson', views.lesson, name = 'lesson'),
+    path('pdf', views.pdf, name = 'pdf'),
+    path('profil', views.profil, name = 'profil'),
+    path('quizz', views.quizz, name = 'quizz'),
 ]
 
--- nanspecialisation/specialisation/views.py
+++ nanspecialisation/specialisation/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def islogin(request):
+    return render(request, 'pages/login.html')
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+def coding(request):
+    return render(request, 'pages/coding.html')
+
+def epreuve(request):
+    return render(request, 'pages/epreuve.html')
+
+def lesson(request):
+    return render(request, 'pages/lesson.html')
+
+def pdf(request):
+    return render(request, 'pages/pdf.html')
+
+def profil(request):
+    return render(request, 'pages/profil.html')
+
+def quizz(request):
+    return render(request, 'pages/quizz.html')
