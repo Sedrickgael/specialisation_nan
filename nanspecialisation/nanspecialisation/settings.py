c36ab6538374813b1b7b19aa523a50f15fbcc28d
Gbane26 <34173224+Gbane26@users.noreply.github.com>
2019-11-19 15:23:24 +0000

    seting arrang

--- nanspecialisation/nanspecialisation/settings.py
+++ nanspecialisation/nanspecialisation/settings.py
@@ -167,36 +167,6 @@ DEFAULT = {
     'width': 'auto',
     'height': 360,
 }
-## Config 2
-
-TINYMCE_DEFAULT_CONFIG = {
-    'height': 360,
-    'width': 900,
-    'cleanup_on_startup': True,
-    'custom_undo_redo_levels': 20,
-    'selector': 'textarea',
-    'theme': 'modern',
-    'plugins': '''
-            textcolor save link image media preview codesample contextmenu
-            table code lists fullscreen  insertdatetime  nonbreaking
-            contextmenu directionality searchreplace wordcount visualblocks
-            visualchars code fullscreen autolink lists  charmap print  hr
-            anchor pagebreak
-            ''',
-    'toolbar1': '''
-            fullscreen preview bold italic underline | fontselect,
-            fontsizeselect  | forecolor backcolor | alignleft alignright |
-            aligncenter alignjustify | indent outdent | bullist numlist table |
-            | link image media | codesample |
-            ''',
-    'toolbar2': '''
-            visualblocks visualchars |
-            charmap hr pagebreak nonbreaking anchor |  code |
-            ''',
-    'contextmenu': 'formats | link image',
-    'menubar': True,
-    'statusbar': True,
-}
 
 FILEBROWSER_DIRECTORY = ''
 FILEBROWSER_MAX_UPLOAD_SIZE = 10485760
