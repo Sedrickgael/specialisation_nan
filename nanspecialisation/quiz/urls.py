a244b9f0034e1f034ff7774672796593024b87f0
Gbane26 <34173224+Gbane26@users.noreply.github.com>
2019-11-20 10:12:20 +0000

    just

--- nanspecialisation/quiz/urls.py
+++ nanspecialisation/quiz/urls.py
@@ -4,11 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.connexion, name='login'),
+    
     path('post', views.islogin, name='login_request'),
     path('home', views.home, name='home'),
     path('profil', views.profil, name='profil'),
     path('quizz', views.quizz, name='quizz'),
     path('epreuve', views.epreuve, name='epreuve'),
     path('lesson', views.lesson, name='lesson'),
+    path('pdf', views.pdf, name = 'pdf'),
+    
     path('deconnexion', views.deconnexion, name='deconnexion'),
 ]
